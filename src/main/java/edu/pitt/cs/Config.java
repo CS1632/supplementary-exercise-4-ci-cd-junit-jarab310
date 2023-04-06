fef076e57b0d805a1d5a2fd3a4667238d4209fee
jarab310 <jar310@pitt.edu>
2023-04-05 22:51:21 -0400

    oops that was from deliverable 2
    now it's exercise 2's src folder

--- src/main/java/edu/pitt/cs/Config.java
+++ src/main/java/edu/pitt/cs/Config.java
@@ -2,28 +2,12 @@ package edu.pitt.cs;
 
 public class Config {
 	// Whether to intentionally inject bugs into CoffeeMakerQuest.
-	private static boolean buggyCoffeeMakerQuest = false;
-	// Whether to intentionally inject bugs into Player.
-	private static boolean buggyPlayer = false;
-	// Whether to intentionally inject bugs into Room.
-	private static boolean buggyRoom = false;
+	private static boolean buggyRentACat = false;
 	
-	public static void setBuggyCoffeeMakerQuest(boolean val) {
-		buggyCoffeeMakerQuest = val;
+	public static void setBuggyRentACat(boolean val) {
+		buggyRentACat = val;
 	}
-	public static void setBuggyPlayer(boolean val) {
-		buggyPlayer = val;
-	}
-	public static void setBuggyRoom(boolean val) {
-		buggyRoom = val;
-	}
-	public static boolean getBuggyCoffeeMakerQuest() {
-		return buggyCoffeeMakerQuest;
-	}
-	public static boolean getBuggyPlayer() {
-		return buggyPlayer;
-	}
-	public static boolean getBuggyRoom() {
-		return buggyRoom;
+	public static boolean getBuggyRentACat() {
+		return buggyRentACat;
 	}
 }
